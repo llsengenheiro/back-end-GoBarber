a0f2d9df308506b0ab93270744ab22e203a039c8
lls.engenheiro <lls.engenheiro@gmail.com>
2019-07-16 10:01:36 -0300

    finalizado parte 01 da api do GoBarber

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup'; // utilizado quando a lib não exporta nenhuma arquivo coloca-se o * as para estanciar em uma variável
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6)
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation error' });
+    }
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -17,9 +31,23 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.body);
-
-    const { email, oldPassword } = req.boby;
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      )
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation error' });
+    }
+    const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
 
