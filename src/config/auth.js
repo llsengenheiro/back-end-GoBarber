58b35c65f716a6968bfdeefe044f48097b90a994
lls.engenheiro <lls.engenheiro@gmail.com>
2019-07-16 08:46:42 -0300

    Erro no update

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'fea645f445063c450e20d131b0a41a44',
+  expiresIn: '7d'
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,27 @@
-import { Router } from "express";
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+/*
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas Lim',
+    email: 'teste@gmail.com',
+    password_hash: '1234567'
+  });
+  return res.json(user);
+});
+*/
 export default routes;
