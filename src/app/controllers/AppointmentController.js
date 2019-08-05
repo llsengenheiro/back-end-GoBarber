b14534bdd7dfcaca898c55d6fd2209e9193f19f2
lls.engenheiro <lls.engenheiro@gmail.com>
2019-08-05 14:54:39 -0300

    redis ok, gerenciando fila de email de cancelamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,7 +5,9 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+
+import CacellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -144,22 +146,10 @@ class AppointmentController {
 
     appointment.canceled_at = new Date();
     await appointment.save();
-    try {
-      await Mail.sendMail({
-        to: `${appointment.provider.name} <${appointment.provider.email}`,
-        subject: 'Agendamento cancelado',
-        template: 'cancellation',
-        context: {
-          provider: appointment.provider.name,
-          user: appointment.user.name,
-          date: format(appointment.date, "dd 'de' MMMM', às' H:mm'h'", {
-            locale: pt
-          })
-        }
-      });
-    } catch (e) {
-      console.log(e);
-    }
+
+    await Queue.add(CacellationMail.key, {
+      appointment
+    });
 
     return res.json(appointment);
   }
